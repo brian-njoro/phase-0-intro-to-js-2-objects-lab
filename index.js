095990787e4cb97a8ba9c055ba38f53b25e7513a
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-15 22:51:08 +0300

    add respective changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+const employee = {
+    name: `Brian`,
+    streetAddress: `2622 sukari`
+}
+
+// update employee without changing original
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee [key] = value
+    return newEmployee
+}
+
+// destructively change employee details
+function destructivelyUpdateEmployeeWithKeyAndValue () {
+    return employee.name = `sam` , employee.streetAddress = `12 Broadway`
+}
+
+// delete employee details without changing original
+function deleteFromEmployeeByKey(employee, key) {
+    const anotherEmployee = {...employee} 
+    delete anotherEmployee.key
+     return anotherEmployee
+}
+
+// destructively delete employee details
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    return delete employee.key
+}
+console.log(destructivelyDeleteFromEmployeeByKey(employee, `name`))
+console.log(employee)
+
+
